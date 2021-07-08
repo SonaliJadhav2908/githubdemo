6efa4457337de12ca575ac7541696a042a9ce236
SonaliJadhav2908 <59473755+SonaliJadhav2908@users.noreply.github.com>
2021-07-08 14:28:49 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+#add implementation
+def add(x,y):
+	pass
+	
+#subtract implementation
+def subtract(x,y)
+	return x-y          # On branch bug456
+
+#multiply implementation	
+def multiply(x,y):
+	return x * y          # on bug456 branch
+	
+#divide implementation	
+def divide(x,y):
+	return x/y            # on bug789 branch
+
+#Sqaure implementation
+def square(x):
+	pass 
