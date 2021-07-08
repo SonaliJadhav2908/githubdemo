db610fcbe4651b0044f43d68466b5b44802eddcc
SonaliJadhav2908 <sonaalijaadhav@gmail.com>
2021-07-08 14:38:05 +0530

    Added description

--- math.py
+++ math.py
@@ -1,16 +1,20 @@
 #add implementation
+# perform addition
 def add(x,y):
 	pass
 	
 #subtract implementation
+# perform subtraction
 def subtract(x,y)
 	return x-y          # On branch bug456
 
-#multiply implementation	
+#multiply implementation
+# perform multiplication
 def multiply(x,y):
 	return x * y          # on bug456 branch
 	
-#divide implementation	
+#divide implementation
+# perform division	
 def divide(x,y):
 	return x/y            # on bug789 branch
 
